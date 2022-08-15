651c48e7b7ddda81094f51b76057150021a6d9e2
ys_sunp <108672163+younsunchae@users.noreply.github.com>
2022-08-15 23:21:50 +0900

    Update 1. 문자찾기.java

--- inflearn/string/1. 문자찾기.java
+++ inflearn/string/1. 문자찾기.java
@@ -29,3 +29,38 @@ public class 문자찾기 {
 	}
 
 }
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	package String;
+
+import java.util.Scanner;
+
+public class 문자찾기2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.next();
+		char a = sc.next().charAt(0);
+		
+		input = input.toLowerCase();
+		a = Character.toLowerCase(a);
+		
+		int count =0;
+		for (int i=0; i<input.length(); i++)  {
+			if(input.charAt(i)==a) {
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+		
+		
+		
+
+	}
+
+}
+
