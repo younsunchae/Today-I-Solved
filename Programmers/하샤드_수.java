bba6c207c5de83e2a4b8f38f2496de14793c6b3b
채윤선 <yoonsun0809@gmail.com>
2022-12-20 23:27:28 +0900

    1220

---
+++ Programmers/하샤드_수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int num = x;
+        int sum = 0;
+        while (num >= 1) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        if (x % sum != 0) {
+            answer = false;
+        }
+        return answer;
+    }
+}
