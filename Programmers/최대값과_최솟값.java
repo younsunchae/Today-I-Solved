f41f62055ee0c7fa121f66d3c55aa349505cfdcf
채윤선 <yoonsun0809@gmail.com>
2023-01-03 23:21:17 +0900

    0103

---
+++ Programmers/최대값과_최솟값.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String [] arr = s.split(" ");
+        int [] nums = new int [arr.length];
+        
+        for (int i =0; i<nums.length; i++){
+            nums[i] = Integer.parseInt(arr[i]);
+        }
+        
+        Arrays.sort(nums);
+        answer = nums[0] + " " + nums[nums.length-1];
+        return answer;
+    }
+}public class 최대값과_최솟값 {
+    
+}
