1f4c4936c81b08846e24842ef2791b670e4b3f55
채윤선 <yoonsun0809@gmail.com>
2023-02-13 23:42:20 +0900

    0213

---
+++ BOJ/25304 영수증.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int total = sc.nextInt();
+        int N = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum += a * b;
+
+        }
+
+        if (total == sum)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+    }
+
+}
