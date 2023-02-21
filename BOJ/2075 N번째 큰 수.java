dd92eda1e1baa695f1ccc67b45e9f8fa621f848a
채윤선 <yoonsun0809@gmail.com>
2023-02-22 00:42:44 +0900

    0221

---
+++ BOJ/2075 N번째 큰 수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                q.offer(num);
+            }
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            q.poll();
+        }
+
+        System.out.println(q.poll());
+
+    }
+
+}
