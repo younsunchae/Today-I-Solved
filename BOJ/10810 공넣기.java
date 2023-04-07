d4ef992bfc74921d7b9955013de0d9a10f6a366d
채윤선 <yoonsun0809@gmail.com>
2023-04-08 03:09:19 +0900

    0407

---
+++ BOJ/10810 공넣기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] map = new int[N + 1];
+
+        for (int a = 0; a < M; a++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            for (int x = i; x <= j; x++) {
+                map[x] = k;
+            }
+        }
+
+        for (int a = 1; a <= N; a++) {
+            System.out.print(map[a] + " ");
+        }
+
+    }
+
+}
